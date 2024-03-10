30ada9c9209ff9e94885b76ea1a5c207226b3c04
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-03-09 23:01:14 -0300

    permite salvar o resultado num relatório .csv

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 require_once 'autoload.php';
 require_once 'config.php';
 
+use Src\Helpers\File;
 use Src\Helpers\Time;
 use Src\Playground;
 use Src\PostgresRepository;
 
 $ln = '';
-$R  = 100;
-$B  = 300;
+$R  = 10;
+$B  = 30;
 
 $playground = new Playground(new PostgresRepository());
 
@@ -19,20 +20,37 @@ $playground->runMigrations();
 
 echo 'Banco de dados e tabela criados.' . PHP_EOL;
 
-foreach(['runInsert', 'runUpdate', 'runSelect'] as $operation) {
+$report = new File();
+
+$pipeline = [
+    'inserts' => 'runInsert',
+    'updates' => 'runUpdate',
+    'selects' => 'runSelect',
+];
+
+foreach($pipeline as $name => $operation) {
     $time = new Time();
     $pk = 1;
 
-    echo  PHP_EOL . 'Iniciando ' . $operation .'...' . PHP_EOL;
+    echo  PHP_EOL . 'Iniciando: ' . $name .'...' . PHP_EOL;
+
+    $report->putLine("r;$name(s)");
 
     $time->startTime();
+
     foreach(range(1, $R) as $it_r) {
         foreach(range(1, $B) as $it_b) {
             $playground->{$operation}($pk);
+            $time->endTime();
+            $m = $time->elapsedTime()/1e9/$B;
+            $report->putLine("$it_r;$m");
             $pk++;
         }
     }
+
     $time->endTime();
 
-    echo 'Finalizado:  ' . $operation .'. Em: ' . $time->elapsedTime() . ' s' . PHP_EOL;
+    echo 'Finalizado:  ' . $name .'. Em: ' . $time->elapsedTime() . ' s' . PHP_EOL;
 }
+
+$report->save();
---
+++ src/Helpers/File.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Src\Helpers;
+
+class File
+{
+    public const FILE_NAME = 'out.csv';
+
+    private string $content = '';
+
+    public function putLine(string $line): void
+    {
+        $this->content .= $line . "\n";
+    }
+
+    public function save(): void
+    {
+        if (file_exists(self::FILE_NAME)) {
+            unlink(self::FILE_NAME);
+        }
+
+        file_put_contents(self::FILE_NAME, $this->content);
+    }
+}
